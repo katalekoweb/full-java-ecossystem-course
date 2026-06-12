acaa508d84831bb96b3b44e2c90050ddb5746ac1
Julião Kataleko <katalekoweb@gmail.com>
2026-06-12 09:20:06 +0100

    feat: comparing operators

---
+++ 1.java-basics/5.comparing/Comparing.java
@@ -0,0 +1,28 @@
+class Comparing 
+{
+    public static void main (String args[])
+    {
+        int x = 7;
+        int y = 5;
+        int a = 5;
+        int b = 9;
+
+        /**
+         * Relational Operators
+         * ==       -> equal
+         * <        -> less than
+         * >        -> greater than
+         * !=       -> not equal
+         * <=       -> less or equal
+         * >=       -> greater or equal
+         * 
+         * Logical Operators
+         * &&       -> and
+         * ||       -> or
+         * !        -> not
+         */
+
+        boolean result = (a < b) && (x > y);
+        System.out.println(result);
+    }
+}
