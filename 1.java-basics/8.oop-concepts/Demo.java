045e1b147822413b5effc73204ec07b5fc70407d
Julião Kataleko <katalekoweb@gmail.com>
2026-06-13 12:09:30 +0100

    feat: data areas

--- 1.java-basics/8.oop-concepts/Demo.java
+++ 1.java-basics/8.oop-concepts/Demo.java
@@ -1,21 +1,53 @@
 class Demo
 {
     public static void main (String args[]) {
-        int num1 = 4, num2 = 5; // primitives
+        // Computer computer = new Computer();
+        // computer.playMusic();
+        // String pen = computer.getMeAPen(12.5);
+        // System.out.println(pen);
 
-        Calculator calculator = new Calculator(); // reference vars
-        int result = calculator.add(num1, num2);
+        int data = 10; // primitives go to the stack memory
 
-        // int result = num1 + num2;
-        System.out.println(result);
+        Calculator obj1 = new Calculator(); 
+        Calculator obj2 = new Calculator();
+        // the objects are created on the heap memory
+        int r1 = obj1.add(3, 4, 9);
+        // System.out.println(r1);
+
+        System.out.println(obj1.num);
+        System.out.println(obj2.num);
     }
 }
 
-class Calculator {
-    int a;
-    public int add (int n1, int n2) {
-        int r = n1+n2;
-        return r;
+class Calculator
+{
+    int num = 10; // instance variable - this vars are on stack memory
+
+    public int add (int n1, int n2)
+    {
+        int result = n1+n2;
+        return result;
+    }
+
+    public int add (int n1, int n2, int n3)
+    {
+        // System.out.println(num);
+        int result = n1+n2+n3;
+        return result;
+    }
+}
+
+class Computer
+{
+    public void playMusic () {
+        System.out.println("Music Playing...");
+    }
+
+    public String getMeAPen (double cost) {
+        if (cost >= 10)
+            return "Pen";
+
+        return "Nothing";
     }
 }
 
--- Timelog.md
+++ Timelog.md
@@ -5,3 +5,5 @@ About Java, Hello world, Variables, Min: 1h:04
 
 ## Day 2 - 12-06-2026
 Type conversion and casting, operators,comparing, conditionals, loops, oops Min: 03:12:08
+
+## Day 3 - 13-06-2026
