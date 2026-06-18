4a507a0bb7909c0332ca59cf064fab69c2cb8fe8
Julião Kataleko <katalekoweb@gmail.com>
2026-06-18 06:55:38 +0100

    feat: methods overloading

---
+++ 1.java-core/14.method-overloading/Demo.java
@@ -0,0 +1,38 @@
+public class Demo {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.show();
+        obj.config();
+    }
+}
+
+class A
+{
+    public void show () {
+        System.out.println("In A show");
+    }
+
+    public void config () {
+        System.out.println("In A config");
+    }
+}
+
+class B extends A
+{
+    @Override
+    public void show() {
+        System.out.println("In B show");
+    }
+}
+
+class Calc {
+    public int add (int n1, int n2) {
+        return n1+n2;
+    }
+}
+
+class AdvCalc extends Calc {
+    public int add (int n1, int n2) {
+        return n1+n2+1;
+    }
+}
