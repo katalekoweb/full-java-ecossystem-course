0bac8f780b29a15d1dc2d06e2a606e0cd19893c3
Julião Kataleko <katalekoweb@gmail.com>
2026-06-18 13:52:13 +0100

    feat: constants

---
+++ 1.java-core/16.constants/Demo.java
@@ -0,0 +1,29 @@
+public class Demo {
+    public static void main(String[] args) {
+        final int PIE = 8; // the value is imutable
+        // num = 10;
+        System.out.println(PIE);
+
+        Calc calc = new Calc();
+        calc.show();
+        calc.add(10, 15);
+    }
+}
+
+final class Calcul
+{
+    public void show () 
+    {
+ 
+    }
+
+    public void add(int a, int b)
+    {
+        System.out.println(a+b);
+    }
+}
+
+class AdvCalc extends Calcul
+{
+
+}
